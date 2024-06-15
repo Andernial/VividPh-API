13c746d7ea110cc6f6e7dc544b31d5c282f282df
Anderson <loonander@gmail.com>
2024-06-15 17:53:28 -0300

    implementing cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import { testConnection } from './database/connect.js'
 import routers from './routes/index.routes.js'
@@ -7,7 +8,7 @@ const port = 3000
 
 dotenv.config()
 App.use(express.json())
-
+App.use(cors())
 App.use(routers)
 
 App.listen(port,()=>{
