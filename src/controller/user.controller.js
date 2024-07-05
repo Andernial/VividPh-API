9fd21e98258397f51ee7000a7d87d6b2fff5cae2
Anderson <loonander@gmail.com>
2024-07-05 15:47:54 -0300

    find user by name route and update description

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -13,7 +13,7 @@ const userController = {
       if (err) {
         return res.status(400).json(err.message);
       }
-      res.status(201).json({  message: 'Usuário criado com sucesso!' })
+      res.status(201).json({ message: 'Usuário criado com sucesso!' })
     })
   },
 
@@ -23,22 +23,35 @@ const userController = {
       if (err) {
         return res.status(400).send('Erro ao buscar usuários')
       }
-      res.status(200).json({results})
+      res.status(200).json({ results })
+    })
+  },
+
+  getUserByName: (req, res) => {
+
+    const { name } = req.params
+    console.log(name)
+
+    UserService.GetUserByNameService(name,(err, results) => {
+      if (err) {
+        return res.status(400).json(err.message)
+      }
+      res.status(200).json({ results })
     })
   },
 
   login: (req, res) => {
     const { email, password } = req.body
 
-    if(!email||!password){
-      return res.status(400).json({erro:'dados faltando!'})
+    if (!email || !password) {
+      return res.status(400).json({ erro: 'dados faltando!' })
     }
 
-    UserService.Login(email,password,(err, results) => {
+    UserService.Login(email, password, (err, results) => {
       if (err) {
-        return res.status(400).json({err})
+        return res.status(400).json({ err })
       }
-      res.status(200).json({results})
+      res.status(200).json({ results })
     })
   },
 
@@ -46,44 +59,44 @@ const userController = {
     const { imageId, imageUrl, } = req.body
     const id = req.userid
 
-    if(!imageId||!imageUrl){
-      return res.status(400).json({erro:'dados faltando!'})
+    if (!imageId || !imageUrl) {
+      return res.status(400).json({ erro: 'dados faltando!' })
     }
 
-    UserService.CreateProfilePicService(id,imageId,imageUrl,(err, results) => {
+    UserService.CreateProfilePicService(id, imageId, imageUrl, (err, results) => {
       if (err) {
-        return res.status(400).json({err})
+        return res.status(400).json({ err })
       }
-      res.status(200).json({results})
+      res.status(200).json({ results })
     })
   },
 
 
   updateUser: (req, res) => {
-    const  id  = req.userid
-    const { name, password, email } = req.body
+    const id = req.userid
+    const { name, password, email, description } = req.body
 
 
-    if (!name && !password && !email){return res.status(400).json({message: 'Nenhum valor a ser alterado'})}
+    if (!name && !password && !email && !description) { return res.status(400).json({ message: 'Nenhum valor a ser alterado' }) }
 
-      UserService.UpdateUserService(id,name,password,email, (err, results) => {
-        if (err) {
-          return res.status(400).json({message: err.message})
-        }
-        res.status(200).json({ results })
-      })
+    UserService.UpdateUserService(id, name, password, email, description, (err, results) => {
+      if (err) {
+        return res.status(400).json({ message: err.message })
+      }
+      res.status(200).json({ results })
+    })
   },
 
   deleteUser: (req, res) => {
-    const  id  = req.userid
+    const id = req.userid
 
 
-      UserService.DeleteUserService(id, (err, results) => {
-        if (err) {
-          return res.status(400).send(err)
-        }
-        res.status(200).json({ message:"usuário deletado com sucesso!" })
-      })
+    UserService.DeleteUserService(id, (err, results) => {
+      if (err) {
+        return res.status(400).send(err)
+      }
+      res.status(200).json({ message: "usuário deletado com sucesso!" })
+    })
   },
 
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -15,21 +15,21 @@ class UserService {
       if (err) {
         return callback(err);
       }
-  
+
       if (nameResults.length > 0) {
         return callback(new Error('Nome de usuário já está em uso'));
       }
-  
+
       const checkEmailQuery = 'SELECT * FROM users WHERE email = ?';
       connection.query(checkEmailQuery, [email], (err, emailResults) => {
         if (err) {
           return callback(err);
         }
-  
+
         if (emailResults.length > 0) {
           return callback(new Error('Email já está em uso'));
         }
-  
+
         const insertQuery = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
         connection.query(insertQuery, [name, email, password], (err, results) => {
           if (err) {
@@ -54,57 +54,89 @@ class UserService {
 
   }
 
-  static CreateProfilePicService(userId, publicId, url, callback) {
+  static GetUserByNameService(name, callback) {
     createUsersTable();
     createProfilePicsTable();
 
-    const checkExistingQuery = 'SELECT id FROM profile_pics WHERE user_id = ?';
-    connection.query(checkExistingQuery, [userId], function(err, existingResult) {
+    const query = 'SELECT name, description, id FROM users WHERE name = ?';
+    
+    connection.query(query, [name], (err, userResults) => {
         if (err) {
             return callback(err);
         }
 
-        if (existingResult.length > 0) {
-            const deleteQuery = 'DELETE FROM profile_pics WHERE user_id = ?';
-            connection.query(deleteQuery, [userId], function(err, deleteResult) {
-                if (err) {
-                    return callback(err);
-                }
-                insertProfilePic(userId, publicId, url, callback);
-            });
-        } else {
-            insertProfilePic(userId, publicId, url, callback);
+        if (userResults.length === 0) {
+            return callback(new Error('Usuário não encontrado'));
         }
-    });
 
+        const user = userResults[0];
 
-    function insertProfilePic(userId, publicId, url, callback) {
-        const insertQuery = 'INSERT INTO profile_pics(user_id, publicId, url) VALUES (?, ?, ?)';
+        const profilePicQuery = 'SELECT publicId, url FROM profile_pics WHERE user_id = ?';
         
-
-        connection.query(insertQuery, [userId, publicId, url], function(err, insertResult) {
+        connection.query(profilePicQuery, [user.id], (err, profilePicResults) => {
             if (err) {
                 return callback(err);
             }
 
-            const selectQuery = `
-            SELECT u.name AS user_name, i.publicId, i.created_at, i.updated_at
-            FROM profile_pics pp
-            JOIN users u ON pp.user_id = u.id
-            JOIN images i ON pp.publicId = i.publicId
-            WHERE pp.user_id = ? AND pp.publicId = ?
-            `;
+            if (profilePicResults.length > 0) {
+                user.profilePic = profilePicResults[0];
+            }
 
-            connection.query(selectQuery, [userId, publicId], function(err, selectResult) {
-                if (err) {
-                    return callback(err);
-                }
+            delete user.id;
 
-                const row = selectResult && selectResult.length > 0 ? selectResult[0] : null;
-                callback(null, row);
-            });
+            callback(null, user);
         });
-    }
+    });
+}
+
+static CreateProfilePicService(userId, publicId, url, callback) {
+  createUsersTable();
+  createProfilePicsTable();
+
+  const checkExistingQuery = 'SELECT id FROM profile_pics WHERE user_id = ?';
+  connection.query(checkExistingQuery, [userId], function (err, existingResult) {
+      if (err) {
+          return callback(err);
+      }
+
+      if (existingResult.length > 0) {
+          const deleteQuery = 'DELETE FROM profile_pics WHERE user_id = ?';
+          connection.query(deleteQuery, [userId], function (err, deleteResult) {
+              if (err) {
+                  return callback(err);
+              }
+              insertProfilePic(userId, publicId, url, callback);
+          });
+      } else {
+          insertProfilePic(userId, publicId, url, callback);
+      }
+  });
+
+  function insertProfilePic(userId, publicId, url, callback) {
+      const insertQuery = 'INSERT INTO profile_pics(user_id, publicId, url) VALUES (?, ?, ?)';
+      
+      connection.query(insertQuery, [userId, publicId, url], function (err, insertResult) {
+          if (err) {
+              return callback(err);
+          }
+
+          const selectQuery = `
+          SELECT u.name AS user_name, pp.publicId, pp.url, pp.created_at, pp.updated_at
+          FROM profile_pics pp
+          JOIN users u ON pp.user_id = u.id
+          WHERE pp.user_id = ? AND pp.publicId = ?
+          `;
+
+          connection.query(selectQuery, [userId, publicId], function (err, selectResult) {
+              if (err) {
+                  return callback(err);
+              }
+
+              const row = selectResult && selectResult.length > 0 ? selectResult[0] : null;
+              callback(null, row);
+          });
+      });
+  }
 }
 
 
@@ -112,58 +144,58 @@ class UserService {
 
 
 
-static Login(email, password, callback) {
-  createProfilePicsTable();
-  createUsersTable();
+  static Login(email, password, callback) {
+    createProfilePicsTable();
+    createUsersTable();
 
-  const userQuery = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    const userQuery = 'SELECT * FROM users WHERE email = ? AND password = ?';
 
-  connection.query(userQuery, [email, password], (err, results) => {
+    connection.query(userQuery, [email, password], (err, results) => {
       if (err) {
-          return callback(err);
+        return callback(err);
       }
 
       if (results.length === 0) {
-          return callback(new Error('Usuário não encontrado'));
+        return callback(new Error('Usuário não encontrado'));
       }
 
       const userExists = results[0];
 
       const token = jwt.sign(
-          { userid: userExists.id, role: 'user' },
-          SECRET,
-          { expiresIn: '10h' }
+        { userid: userExists.id, role: 'user' },
+        SECRET,
+        { expiresIn: '10h' }
       );
 
       const object = {
-          token: token,
-          name: userExists.name,
-          email: userExists.email,
-          id: userExists.id,
-          auth: true
+        token: token,
+        name: userExists.name,
+        email: userExists.email,
+        id: userExists.id,
+        auth: true
       };
 
       const profilePicQuery = 'SELECT publicId, url FROM profile_pics WHERE user_id = ?';
 
       connection.query(profilePicQuery, [userExists.id], (err, picResults) => {
-          if (err) {
-              return callback(err);
-          }
+        if (err) {
+          return callback(err);
+        }
 
-          if (picResults.length > 0) {
-              object.profilePic = {
-                  publicId: picResults[0].publicId,
-                  url: picResults[0].url
-              };
-          }
+        if (picResults.length > 0) {
+          object.profilePic = {
+            publicId: picResults[0].publicId,
+            url: picResults[0].url
+          };
+        }
 
-          callback(null, object);
+        callback(null, object);
       });
-  });
-}
+    });
+  }
 
 
-  static UpdateUserService(id, name, password, email, callback) {
+  static UpdateUserService(id, name, password, email, description, callback) {
     createUsersTable();
 
     const userExists = 'SELECT * FROM users WHERE id = ?';
@@ -191,6 +223,11 @@ static Login(email, password, callback) {
       if (email) {
         fieldsToUpdate.push('email = ?');
         values.push(email);
+      }
+
+      if (description) {
+        fieldsToUpdate.push('description = ?');
+        values.push(description);
       }
 
       values.push(id);
